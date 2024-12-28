dab31d30541299e600fe8f0f7448af893828a638
SlayZ121 <slayz9168@gmail.com>
2024-12-28 22:14:41 +0530

    egg class

--- script.js
+++ script.js
@@ -166,6 +166,41 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Egg {
+    constructor(game) {
+      this.game = game;
+      this.collisionx = Math.random() * this.game.width;
+      this.collisiony = Math.random() * this.game.height;
+      this.collisionRadius = 40;
+      this.image = document.getElementById("egg");
+      this.spriteWidth = 110;
+      this.spriteHeight = 135;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.spriteX = this.collisionx + this.width * 0.5;
+      this.spriteY = this.collisiony + this.height * 0.5;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.spriteX, this.spriteY);
+      if (this.game.debug) {
+        context.beginPath();
+        context.arc(
+          this.collisionx,
+          this.collisiony,
+          this.collisionRadius,
+          0,
+          Math.PI * 2
+        );
+
+        context.save();
+        context.globalAlpha = 0.5;
+        context.fill();
+        context.restore();
+        context.stroke();
+      }
+    }
+  }
+
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
@@ -178,7 +213,9 @@ window.addEventListener("load", function () {
       this.timer = 0;
       this.interval = 1000 / this.fps;
       this.numOfObstacles = 10;
+      this.maxEggs = 10;
       this.obstacles = [];
+      this.eggs = [];
       this.mouse = {
         x: this.width * 0.5,
         y: this.height * 0.5,
@@ -223,6 +260,8 @@ window.addEventListener("load", function () {
       const sumOfRadii = a.collisionRadius + b.collisionRadius;
       return [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
     }
+
+    addEgg() {}
 
     init() {
       let attempts = 0;
